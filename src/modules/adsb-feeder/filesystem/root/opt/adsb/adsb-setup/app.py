fd56feb0ad4a9ccd982bb55d7ebe1e4867d22db2
Dirk Hohndel <dirk@hohndel.org>
2024-06-28 20:08:15 -0700

    reduce the risk of a startup race condition
    This needs a far more complex solution, but for now it seems not starting
    the background tasks until after the initial setup is sufficient to avoid
    constantly hitting the config.js overwrite race.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- src/modules/adsb-feeder/filesystem/root/opt/adsb/adsb-setup/app.py
+++ src/modules/adsb-feeder/filesystem/root/opt/adsb/adsb-setup/app.py
@@ -406,15 +406,6 @@ class AdsbIm:
 
     def run(self, no_server=False):
         debug = os.environ.get("ADSBIM_DEBUG") is not None
-        # in no_server mode we want to exit right after the housekeeping, so no
-        # point in running this in the background
-        if not no_server:
-            self.update_dns_state()
-            self._dns_watch = Background(3600, self.update_dns_state)
-            if self._d.is_enabled("stage2"):
-                # let's make sure we tell the micro feeders every ten minutes that
-                # the stage2 is around, looking at them
-                self._stage2_checks = Background(600, self.stage2_checks)
         # prepare for app use (vs ADS-B Feeder Image use)
         # newer images will include a flag file that indicates that this is indeed
         # a full image - but in case of upgrades from older version, this heuristic
@@ -469,6 +460,14 @@ class AdsbIm:
         site_name = self._d.env_by_tags("site_name").list_get(0)
         if site_name and os_flag_file.exists():
             self.mdns_alias_service(site_name)
+
+        # finally, set up the background tasks
+        self.update_dns_state()
+        self._dns_watch = Background(3600, self.update_dns_state)
+        if self._d.is_enabled("stage2"):
+            # let's make sure we tell the micro feeders every ten minutes that
+            # the stage2 is around, looking at them
+            self._stage2_checks = Background(600, self.stage2_checks)
 
         self.app.run(
             host="0.0.0.0",
